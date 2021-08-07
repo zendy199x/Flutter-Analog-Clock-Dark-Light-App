6a7d718b8eac334edb1e74d00b7c43bccd55a576
Zendy <nguyenduc.051194@gmail.com>
2021-08-07 23:34:52 +0700

    feat: setup project

---
+++ lib/constants.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+// Colors
+const Color kPrimaryColor = Color(0xFFFF97B3);
+const Color kSecondaryLightColor = Color(0xFFE4E9F2);
+const Color kSecondaryDarkColor = Color(0xFF404040);
+const Color kAccentLightColor = Color(0xFFB3BFD7);
+const Color kAccentDarkColor = Color(0xFF4E4E4E);
+const Color kBackgroundDarkColor = Color(0xFF3A3A3A);
+const Color kSurfaceDarkColor = Color(0xFF222225);
+// Icon Colors
+const Color kAccentIconLightColor = Color(0xFFECEFF5);
+const Color kAccentIconDarkColor = Color(0xFF303030);
+const Color kPrimaryIconLightColor = Color(0xFFECEFF5);
+const Color kPrimaryIconDarkColor = Color(0xFF232323);
+// Text Colors
+const Color kBodyTextColorLight = Color(0xFFA1B0CA);
+const Color kBodyTextColorDark = Color(0xFF7C7C7C);
+const Color kTitleTextLightColor = Color(0xFF101112);
+const Color kTitleTextDarkColor = Colors.white;
+
+const Color kShadowColor = Color(0xFF364564);
---
+++ lib/theme.dart
@@ -0,0 +1,72 @@
+import 'package:analog_clock/constants.dart';
+import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+
+// Our light/Primary Theme
+ThemeData themeData(BuildContext context) {
+  return ThemeData(
+    appBarTheme: appBarTheme,
+    fontFamily: "GoogleSans",
+    primaryColor: kPrimaryColor,
+    // colorSchema.secondary replace accentColor
+    // accentColor: kAccentLightColor,
+    scaffoldBackgroundColor: Colors.white,
+    colorScheme: const ColorScheme.light(
+      primary: kAccentLightColor,
+      secondary: kSecondaryLightColor,
+      // on light theme surface = Colors.white by default
+    ),
+    backgroundColor: Colors.white,
+    iconTheme: const IconThemeData(color: kBodyTextColorLight),
+    // floatingActionButtonTheme replace accentIconTheme
+    // accentIconTheme: const IconThemeData(color: kAccentIconLightColor),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: kAccentIconLightColor,
+    ),
+    primaryIconTheme: const IconThemeData(color: kPrimaryIconLightColor),
+    // textTheme: GoogleFonts.latoTextTheme().copyWith(
+    //   bodyText1: TextStyle(color: kBodyTextColorLight),
+    //   bodyText2: TextStyle(color: kBodyTextColorLight),
+    //   headline4: TextStyle(color: kTitleTextLightColor, fontSize: 32),
+    //   headline1: TextStyle(color: kTitleTextLightColor, fontSize: 80),
+    // ),
+    textTheme: const TextTheme(
+      bodyText1: TextStyle(color: kBodyTextColorLight),
+      bodyText2: TextStyle(color: kBodyTextColorLight),
+      headline4: TextStyle(color: kTitleTextLightColor, fontSize: 32),
+      headline1: TextStyle(color: kTitleTextLightColor, fontSize: 80),
+    ),
+  );
+}
+
+// Dark Them
+ThemeData darkThemeData(BuildContext context) {
+  return ThemeData.dark().copyWith(
+    primaryColor: kPrimaryColor,
+    // accentColor: kAccentDarkColor,
+    scaffoldBackgroundColor: const Color(0xFF0D0C0E),
+    appBarTheme: appBarTheme,
+    colorScheme: const ColorScheme.light(
+      primary: kAccentDarkColor,
+      secondary: kSecondaryDarkColor,
+      surface: kSurfaceDarkColor,
+    ),
+    backgroundColor: kBackgroundDarkColor,
+    iconTheme: const IconThemeData(color: kBodyTextColorDark),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: kAccentIconDarkColor,
+    ),
+    primaryIconTheme: const IconThemeData(color: kPrimaryIconDarkColor),
+    textTheme: const TextTheme(
+      bodyText1: TextStyle(color: kBodyTextColorDark),
+      bodyText2: TextStyle(color: kBodyTextColorDark),
+      headline4: TextStyle(color: kTitleTextDarkColor, fontSize: 32),
+      headline1: TextStyle(color: kTitleTextDarkColor, fontSize: 80),
+    ),
+  );
+}
+
+AppBarTheme appBarTheme = const AppBarTheme(
+  color: Colors.transparent,
+  elevation: 0,
+);
