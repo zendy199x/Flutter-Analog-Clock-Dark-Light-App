42307f61b1ad4bb72424c1ff1372846fc67ef26a
Zendy <nguyenduc.051194@gmail.com>
2021-08-08 00:04:13 +0700

    feat: App Bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,53 @@
+import 'package:analog_clock/size_config.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    // Have to call this on our starting page
+    SizeConfig().init(context);
+    return Scaffold(
+      appBar: buildAppBar(context),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        icon: SvgPicture.asset(
+          "assets/icons/Settings.svg",
+          color: Theme.of(context).iconTheme.color,
+        ),
+        onPressed: () {},
+      ),
+      actions: [
+        buildAddButton(context),
+      ],
+    );
+  }
+
+  Padding buildAddButton(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: getProportionateScreenWidth(10),
+      ),
+      child: InkWell(
+        onTap: () {},
+        child: Container(
+          width: getProportionateScreenWidth(32),
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            shape: BoxShape.circle,
+          ),
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
   }
 }
